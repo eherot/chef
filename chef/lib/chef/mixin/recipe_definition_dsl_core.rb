737294256cd02f57d8f288a16e645c6fcf9c6ef9
Dan DeLeo <danielsdeleo@mac.com>
2009-09-27 02:27:18 -0600

    don't touch others' ivars, its inappropriate
    recipe DSL core uses getter methods so:
    1) these can be delegated/computed/overridden if desired
    2) it can fallback on the basic api it provides to includers

--- chef/lib/chef/mixin/recipe_definition_dsl_core.rb
+++ chef/lib/chef/mixin/recipe_definition_dsl_core.rb
@@ -61,14 +61,14 @@ class Chef
         # If we have a definition that matches, we want to use that instead.  This should
         # let you do some really crazy over-riding of "native" types, if you really want
         # to. 
-        if @definitions.has_key?(method_symbol)
+        if definitions.has_key?(method_symbol)
           # This dupes the high level object, but we still need to dup the params
-          new_def = @definitions[method_symbol].dup
+          new_def = definitions[method_symbol].dup
           new_def.params = new_def.params.dup
-          new_def.node = @node
+          new_def.node = node
           # This sets up the parameter overrides
           new_def.instance_eval(&block) if block
-          new_recipe = Chef::Recipe.new(@cookbook_name, @recipe_name, @node, @collection, @definitions, @cookbook_loader)
+          new_recipe = Chef::Recipe.new(cookbook_name, recipe_name, node, collection, definitions, cookbook_loader)
           new_recipe.params = new_def.params
           new_recipe.params[:name] = args[0]
           new_recipe.instance_eval(&new_def.recipe)
