8c9cd678b672caf5ef3b9854f2a225ca48dd7a0d
Dan DeLeo <danielsdeleo@mac.com>
2009-10-13 18:05:35 -0600

    put DSL examples in DSL's specs

--- chef/spec/unit/mixin/recipe_definition_dsl_core_spec.rb
+++ chef/spec/unit/mixin/recipe_definition_dsl_core_spec.rb
@@ -20,6 +20,9 @@ require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "spec_hel
 
 class RecipeDSLUser
   include Chef::Mixin::RecipeDefinitionDSLCore
+  
+  attr_writer :node
+  
 end
 
 class PseudoResource
@@ -74,6 +77,125 @@ describe Chef::Mixin::RecipeDefinitionDSLCore do
     it "defines a method for a resource" do
       @dsl_core.add_resource_to_dsl(PseudoResource)
       @implementer.should respond_to(:pseudo_resource)
+    end
+
+  end
+  
+  describe "using the DSL" do
+    describe "resources" do
+      it "should load a two word (zen_master) resource" do
+        lambda do
+          @dsl_user.zen_master "monkey" do
+            peace true
+          end
+        end.should_not raise_error(ArgumentError)
+      end
+  
+      it "should load a one word (cat) resource" do
+        lambda do
+          @dsl_user.cat "loulou" do
+            pretty_kitty true
+          end
+        end.should_not raise_error(ArgumentError)
+      end
+      
+      it "should load a four word (one_two_three_four) resource" do 
+        lambda do
+          @dsl_user.one_two_three_four "numbers" do
+            i_can_count true
+          end
+        end.should_not raise_error(ArgumentError)
+      end
+  
+      it "should throw an error and log to fatal if you access an undefined resource" do
+        Chef::Log.should_receive(:fatal)
+        lambda { @dsl_user.not_home { || } }.should raise_error(NameError)
+      end
+  
+      it "should allow regular errors (not NameErrors) to pass unchanged" do
+        lambda { 
+          @dsl_user.cat { || raise ArgumentError, "You Suck" } 
+        }.should raise_error(ArgumentError)
+      end
+  
+      it "should add our zen_master to the collection" do
+        @dsl_user.zen_master "monkey" do
+          peace true
+        end
+        @dsl_user.collection.lookup("zen_master[monkey]").name.should eql("monkey")
+      end
+  
+      it "should add our zen masters to the collection in the order they appear" do
+        %w{monkey dog cat}.each do |name|
+          @dsl_user.zen_master name do
+            peace true
+          end
+        end
+        @dsl_user.collection.each_index do |i|
+          case i
+          when 0
+            @dsl_user.collection[i].name.should eql("monkey")
+          when 1
+            @dsl_user.collection[i].name.should eql("dog")
+          when 2
+            @dsl_user.collection[i].name.should eql("cat")
+          end
+        end
+      end
+        
+      it "should return the new resource after creating it" do
+        res = @dsl_user.zen_master "makoto" do
+          peace true
+        end
+        res.resource_name.should eql(:zen_master)
+        res.name.should eql("makoto")
+      end
+    end
+      
+    describe "resource definitions" do
+      it "should execute defined resources" do
+        crow_define = Chef::ResourceDefinition.new
+        crow_define.define :crow, :peace => false, :something => true do
+          zen_master "lao tzu" do
+            peace params[:peace]
+            something params[:something]
+          end
+        end
+        @dsl_user.crow "mine" do
+          peace true
+        end
+        @dsl_user.collection.resources(:zen_master => "lao tzu").name.should eql("lao tzu")
+        @dsl_user.collection.resources(:zen_master => "lao tzu").something.should eql(true)
+      end
+
+      it "should set the node on defined resources" do
+        @dsl_user.node = Chef::Node.new
+        
+        crow_define = Chef::ResourceDefinition.new
+        crow_define.define :crow, :peace => false, :something => true do
+          zen_master "lao tzu" do
+            peace params[:peace]
+            something params[:something]
+          end
+        end
+        @dsl_user.node[:foo] = false
+        @dsl_user.crow "mine" do
+          something node[:foo]
+        end
+        @dsl_user.collection.resources(:zen_master => "lao tzu").something.should eql(false)
+      end
+    end
+    
+    describe "instance_eval" do
+      it "should handle an instance_eval properly" do
+        code = <<-CODE
+    zen_master "gnome" do
+      peace = true
+    end
+    CODE
+        lambda { @dsl_user.instance_eval(code) }.should_not raise_error
+        @dsl_user.collection.resources(:zen_master => "gnome").name.should eql("gnome")
+      end
     end
 
   end
